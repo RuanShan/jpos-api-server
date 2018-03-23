d893b2740d574b34b29620b6728bce9bb264e540
qinghe <david@getstore.cn>
2018-03-23 13:33:52 +0800

    add env production

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import morgan from 'morgan';
 import db from './mongodb/db.js';
 import config from 'config-lite';
 import router from './routes/index.js';
@@ -13,15 +14,18 @@ import chalk from 'chalk';
 // import Statistic from './middlewares/statistic'
 
 const app = express();
+app.use(morgan('short'));
 
 app.all('*', (req, res, next) => {
 	res.header("Access-Control-Allow-Origin", req.headers.origin || '*');
 	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
 	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  	res.header("Access-Control-Allow-Credentials", true); //可以带cookies
+  res.header("Access-Control-Allow-Credentials", true); //可以带cookies
 	res.header("X-Powered-By", '3.2.1')
+	res.header("Access-Control-Max-Age", '0')
+
 	if (req.method == 'OPTIONS') {
-	  	res.send(200);
+	  	res.sendStatus(200);
 	} else {
 	    next();
 	}
